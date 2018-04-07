b8650740ad2013b3dc0652f669e3f4e8a91d319e
garrrikkotua <36304232+garrrikkotua@users.noreply.github.com>
2018-04-07 15:59:34 +0300

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,18 +1,30 @@
+"""
+This script generates text from model trained by train.py module
+Next word is generated using weighted numpy.random.choice
+with frequencies of words in text
+The program has console interface, implemented with argparse module.
+The first word to generate could be specified by user or
+it will be chosen randomly.
+!Note: if word specified is not presented int the text,
+ValueError will be raised.
+author: Igor Kotua
+version: 1.0.2
+"""
+
+
 import argparse as ap
 import random
 import sys
-from collections import defaultdict
+import numpy
+import pickle
 
 
 def load_model(file):  # loading model
     """
-    :type file: _io.TextIOWrapper
+    :type file: FileIO[bytes]
+    :return dict of dicts of words and frequencies
     """
-    d = defaultdict(dict)
-    for line in file:
-        w1, w2, count = line.split()
-        d[w1][w2] = int(count)
-    return d
+    return pickle.load(file)
 
 
 def get_start_word(model, seed):  # getting first word of the text
@@ -22,24 +34,22 @@ def get_start_word(model, seed):  # getting first word of the text
     """
     if seed == '':
         return random.choice(list(model.keys()))
-    else:
-        if seed not in model.keys():
-            raise ValueError('Invalid seed')
-        return seed
+    if seed not in model.keys():
+        raise ValueError('Invalid seed')
+    return seed
 
 
 def get_next_word(model, current_word):
     """
-        :param model: dict containing model
-        :param current_word: last outputted word in text
-        """
-    total = sum(w for w in model[current_word].values())
-    r = random.uniform(0, total)
-    upto = 0
-    for c, w in model[current_word].items():
-        if upto + w >= r:
-            return c
-        upto += w
+    :param model: dict containing model
+    :param current_word: last outputted word in text
+    """
+    if current_word not in model:  # there are no words after current
+        return numpy.random.choice(list(model.keys()))
+    d = model[current_word]
+    s = sum(d.values())
+    p = [d[i] / s for i in d.keys()]  # probabilities for next word
+    return numpy.random.choice(list(d.keys()), p=p)
 
 
 def generate(file, seed, length, output):
@@ -52,22 +62,31 @@ def generate(file, seed, length, output):
     model = load_model(file)
     current_word = get_start_word(model, seed)
     for i in range(length):
-        output.write('{} '.format(current_word))
-        current_word = get_next_word(model, current_word)
+        if i == length - 1:
+            output.write('{}\n'.format(current_word))
+        else:
+            output.write('{} '.format(current_word))
+            current_word = get_next_word(model, current_word)
 
 
-parse = ap.ArgumentParser(description='Generate some text')
-parse.add_argument('-m', '--model', help='File to read model from',
-                   required=True)
-parse.add_argument('-s', '--seed', help='Word to start generation',
-                   default='')
-parse.add_argument('-l', '--length', type=int, help='Length of text',
-                   required=True)
-parse.add_argument('-o', '--output', help='File to output text', default='')
-args = parse.parse_args()
-with open(args.model, 'r') as in_file:
-    if args.output == '':
-        generate(in_file, args.seed, args.length, sys.stdout)
-    else:
-        with open(args.output, 'w') as out_file:
-            generate(in_file, args.seed, args.length, out_file)
+def main():
+    parse = ap.ArgumentParser(description='Generate some text')
+    parse.add_argument('-m', '--model', help='File to read model from',
+                       required=True)
+    parse.add_argument('-s', '--seed', help='Word to start generation',
+                       default='')
+    parse.add_argument('-l', '--length', type=int, help='Length of text',
+                       required=True)
+    parse.add_argument('-o', '--output', help='File to output text',
+                       default='')
+    args = parse.parse_args()
+    with open(args.model, 'rb') as in_file:
+        if args.output == '':
+            generate(in_file, args.seed, args.length, sys.stdout)
+        else:
+            with open(args.output, 'w') as out_file:
+                generate(in_file, args.seed, args.length, out_file)
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,34 +1,40 @@
+"""
+This script is a simple text generator, which uses bigram models.
+Bigrams frequencies are counted using Counter module.
+Model saves in byte format, using pickle module.
+The program has console interface, implemented with argparse module.
+For detailed information about usage please type -help in console.
+author: Igor Kotua
+version: 1.0.2
+"""
+
+
 import argparse as ap
 import sys
 import re
 from collections import Counter
 import os
+import pickle
 from collections import defaultdict
-
-
-def previous_bigrams(model):  # loads current mode if file is not empty
-    """
-    :type model: str
-    """
-    d = defaultdict(lambda: 0)
-    if os.path.exists(model):
-        with open(model, 'r') as file:
-            for line in file:
-                w1, w2, count = line.rstrip().split()
-                d[(w1, w2)] = int(count)
-    return d
 
 
 def gen_files(input_dir):  # yields file from input-dir
     """
     :param input_dir: directory of input files
     """
-    for dirpath, _, files in os.walk(input_dir):
-        for filename in files:
-            if filename.endswith('.txt'):
-                file = open(os.path.join(dirpath, filename), 'r')
-                yield file
-                file.close()
+    if input_dir == '':
+        yield sys.stdin
+    else:
+        if os.path.isfile(input_dir):  # lone file
+            file = open(input_dir, 'r')
+            yield file
+            file.close()
+        else:
+            for dirpath, _, files in os.walk(input_dir):
+                for filename in files:
+                    file = open(os.path.join(dirpath, filename), 'r')
+                    yield file
+                    file.close()
 
 
 def gen_lines(files, lowercase):  # yields line from file
@@ -36,13 +42,11 @@ def gen_lines(files, lowercase):  # yields line from file
     :param files: iterable object containing files
     :param lowercase: bool saying whether to cast to lower or not
     """
-    if lowercase:
-        for file in files:
-            for line in file:
+    for file in files:
+        for line in file:
+            if lowercase:
                 yield line.lower()
-    else:
-        for file in files:
-            for line in file:
+            else:
                 yield line
 
 
@@ -60,7 +64,7 @@ def gen_bigrams(words, model):  # yields bigrams using words
     """
     :param words: iterable of strings, considered to be words
     """
-    b1 = '*start*'
+    b1 = next(words)
     for b2 in words:
         yield (b1, b2)
         b1 = b2
@@ -73,13 +77,11 @@ def count_freq(bigrams, model):
     :param model: str path to save model
     """
     c = Counter(bigrams)
-    d = previous_bigrams(model)
-    for bigram, count in c.items():  # updating model
-        d[bigram] += count
-    with open(model, 'w') as file:   # writing model to file
-        for bigram in d:
-            w1, w2 = bigram
-            file.write('{} {} {}\n'.format(w1, w2, d[bigram]))
+    d = defaultdict(dict)
+    for a, b in c.keys():  # dict of dicts of words and frequencies
+        d[a][b] = c[(a, b)]
+    with open(model, 'wb') as file:   # writing model to file
+        pickle.dump(d, file)
 
 
 def train(input_dir, model, lowercase):
@@ -88,25 +90,24 @@ def train(input_dir, model, lowercase):
     :param model: str path to save model
     :param lowercase: bool saying whether to cast to lower or not
     """
-    if input_dir != '':
-        files = gen_files(input_dir)
-        lines = gen_lines(files, lowercase)
-        words = gen_words(lines)
-        bigrams = gen_bigrams(words, model)
-        count_freq(bigrams, model)
-    else:   # input-dir is not specified, writing to sys.stdin
-        lines = gen_lines([sys.stdin], lowercase)
-        words = gen_words(lines)
-        bigrams = gen_bigrams(words, model)
-        count_freq(bigrams, model)
+    files = gen_files(input_dir)
+    lines = gen_lines(files, lowercase)
+    words = gen_words(lines)
+    bigrams = gen_bigrams(words, model)
+    count_freq(bigrams, model)
 
 
-parse = ap.ArgumentParser(description='Train on  some text')
-parse.add_argument('-i', '--input-dir', help='Input directory',
-                   default='', required=True)
-parse.add_argument('-m', '--model',
-                   help='Path to the file where model will be saved',
-                   required=True)
-parse.add_argument('--lc', action='store_true', help='Make text lowercase')
-args = parse.parse_args()
-train(args.input_dir, args.model, args.lc)
+def main():
+    parse = ap.ArgumentParser(description='Train on  some text')
+    parse.add_argument('-i', '--input-dir', help='Input directory',
+                       default='', required=True)
+    parse.add_argument('-m', '--model',
+                       help='Path to the file where model will be saved',
+                       required=True)
+    parse.add_argument('--lc', action='store_true', help='Make text lowercase')
+    args = parse.parse_args()
+    train(args.input_dir, args.model, args.lc)
+
+
+if __name__ == '__main__':
+    main()
